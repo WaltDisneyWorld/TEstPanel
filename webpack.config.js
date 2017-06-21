d3e5b9add45031b948cee937541d449b4a0be1de
Matthew Finerty <evilarceus@gmail.com>
2017-06-21 16:26:49 -0700

    Added Webpack config (dev)

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval',
+    entry: {
+        app: ['./app/index.js']
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+        publicPath: 'http://localhost:8080/'
+    },
+    devServer: {
+        contentBase: './app',
+        publicPath: 'http://localhost:8080/'
+    },
+    module: {
+        loaders:
+            [
+                {
+                    test: /\.js?$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    query: {
+                        presets: ['es2015', 'stage-1', 'react']
+                    }
+                },
+                {
+                    test: /\.less$/,
+                    loader: 'style-loader!css-loader!less-loader',
+                    exclude: /node_modules/,
+                    include: path.resolve(__dirname, 'app')
+                }
+            ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "EvilToolkit - Webpack",
+            filename: "app/index.html"
+        }),
+    ]
+};
